6057ecbcfa44eec80070594cf6e637f8ab4a85a2
Imagoiq <imagoiq@posteo.net>
2019-03-29 17:16:48 +0100

    Revert change for test sw

--- src/store/store.js
+++ src/store/store.js
@@ -42,7 +42,7 @@ const getDefaultState = () => {
       running: false,
       number: 0,
       chrono: {
-        init: 10, //sec
+        init: 60, //sec
         bonus: 1, //sec
         penalty: -1, //sec
         penaltyCoefficient: 10,
