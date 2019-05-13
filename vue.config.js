fb9953a63fe2a1a73f2956818945457f0ec7960b
Imagoiq <imagoiq@posteo.net>
2019-05-13 18:42:41 +0200

    Proper use of ENV vars for lang

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: 'fr',
-      fallbackLocale: 'fr',
+      locale: process.env.VUE_APP_I18N_LOCALE,
+      fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE,
       localeDir: 'locales',
       enableInSFC: false
     }
